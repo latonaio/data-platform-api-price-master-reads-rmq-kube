b051ea98d728aa23e9ef62f6f269fa491e47af41
MiyuYokoyama0109 <miyu.y@latona.io>
2022-12-13 16:03:53 +0900

    release

--- DPFM_API_Caller/requests/price_master.go
+++ DPFM_API_Caller/requests/price_master.go
@@ -1,22 +1,22 @@
 package requests
 
 type PriceMaster struct {
-	BusinessPartner            *int     `json:"BusinessPartner"`
+	BusinessPartner            int      `json:"BusinessPartner"`
 	ConditionRecordCategory    string   `json:"ConditionRecordCategory"`
-	ConditionRecord            *int     `json:"ConditionRecord"`
-	ConditionSequentialNumber  *int     `json:"ConditionSequentialNumber"`
+	ConditionRecord            int      `json:"ConditionRecord"`
+	ConditionSequentialNumber  int      `json:"ConditionSequentialNumber"`
 	ConditionType              string   `json:"ConditionType"`
-	ConditionValidityEndDate   *string  `json:"ConditionValidityEndDate"`
-	ConditionValidityStartDate *string  `json:"ConditionValidityStartDate"`
+	ConditionValidityEndDate   string   `json:"ConditionValidityEndDate"`
+	ConditionValidityStartDate string   `json:"ConditionValidityStartDate"`
 	Product                    string   `json:"Product"`
 	Customer                   *int     `json:"Customer"`
 	Supplier                   *int     `json:"Supplier"`
 	CreationDate               *string  `json:"CreationDate"`
 	ConditionRateValue         *float32 `json:"ConditionRateValue"`
-	ConditionRateValueUnit     string   `json:"ConditionRateValueUnit"`
+	ConditionRateValueUnit     *string  `json:"ConditionRateValueUnit"`
 	ConditionRateRatio         *float32 `json:"ConditionRateRatio"`
-	ConditionRateRatioUnit     string   `json:"ConditionRateRatioUnit"`
-	ConditionCurrency          string   `json:"ConditionCurrency"`
-	BaseUnit                   string   `json:"BaseUnit"`
+	ConditionRateRatioUnit     *string  `json:"ConditionRateRatioUnit"`
+	ConditionCurrency          *string  `json:"ConditionCurrency"`
+	BaseUnit                   *string  `json:"BaseUnit"`
 	ConditionIsDeleted         *bool    `json:"ConditionIsDeleted"`
 }
